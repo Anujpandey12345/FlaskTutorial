4a6c192e69090438b027788b8f074af185dcab7c
Anuj Kumar <anujpandey61687@gmail.com>
2026-02-16 11:42:30 +0530

    feat: process form inputs manually using request.form

---
+++ Phase3/app4.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, session, url_for, redirect
+app = Flask(__name__)
+@app.route("/feedback", methods=["GET", "POST"])
+def feedback():
+    if request.method == "POST":
+        name = request.form.get("username")
+        message = request.form.get("message")
+
+
+        return render_template("thankyou.html", user=name, message=message)
+    
+
+    return render_template("feedback.html")
