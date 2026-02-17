98f61c84409e03592ca798a2b77590f38147b7b4
Anuj Kumar <anujpandey61687@gmail.com>
2026-02-17 23:45:01 +0530

    feat: add form validation with custom required field messages

--- Phase3/forms.py
+++ Phase3/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Email, Length
 
 
 class RegistrationForm(FlaskForm):
-    name = StringField("Full Name", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
+    name = StringField("Full Name", validators=[DataRequired(message="We need your name, it cannot be empty!")]) #Custom message for showing 
+    email = StringField("Email", validators=[DataRequired(message="Please enter your mail, you cannot done empty !"), Email()])
+    password = PasswordField("Password", validators=[DataRequired(message="Please enter atleast 8 Digit Password !"), Length(min=8)])
     submit = SubmitField("Register")
